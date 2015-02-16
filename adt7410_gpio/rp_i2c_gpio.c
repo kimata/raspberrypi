68e5cc3ad516072dd0d14d40c18f9717f43d3a3c
KIMATA Tetsuya <kimata@green-rabbit.net>
2015-02-16 22:17:52 +0900

    fix code.

--- adt7410_gpio/rp_i2c_gpio.c
+++ adt7410_gpio/rp_i2c_gpio.c
@@ -1,7 +1,7 @@
 /*
  * Raspberry Pi I2C Utility (using GPIO)
  *
- * Copyright (C) 2014 Tetsuya Kimata <kimata@green-rabbit.net>
+ * Copyright (C) 2015 Tetsuya Kimata <kimata@green-rabbit.net>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
