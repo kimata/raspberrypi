61f56b42537c207bd9917f899c96eb76371d5baf
KIMATA Tetsuya <kimata@green-rabbit.net>
2015-06-04 21:40:26 +0900

    fix code.

--- app/watt_meter/start.sh
+++ app/watt_meter/start.sh
@@ -4,9 +4,10 @@
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
 # published by the Free Software Foundation, version 2.
+CWD=`dirname "${0}"`
 
 PIN_NO_SWITCH=13
-LCD_SCRIPT=~/github/raspberrypi/driver/dev/sc2004cs/sc2004cs.test
+LCD_SCRIPT=${CWD}/../../driver/dev/sc2004cs/sc2004cs.test
 
 echo ${PIN_NO_SWITCH} | sudo tee /sys/class/gpio/export > /dev/null 2>&1
 echo in | sudo tee /sys/class/gpio/gpio${PIN_NO_SWITCH}/direction > /dev/null 2>&1
@@ -26,10 +27,10 @@ if [ ${SWITCH} = "0" ]; then
 else
     # OFF
     sudo ifup wlan0
-    sudo ${LCD_SCRIPT} "Power Meter v ${VERSION}" "MODE: WIFI CLIENT"
+    sudo ${LCD_SCRIPT} "Power Meter v ${VERSION}" "MODE: WIFI CLIENT" "" "http://$(hostname -i)/"
 fi
 
-sudo ./watt_meter
+sudo ${CWD}/watt_meter
 
 # Local Variables:
 # coding: shift_jis-unix
