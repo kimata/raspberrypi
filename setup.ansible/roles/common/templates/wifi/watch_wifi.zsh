5b37dbece46d9b3a2eb1f066fb6b0bdf27b3c34a
KIMATA Tetsuya <kimata@green-rabbit.net>
2019-09-08 20:45:14 +0900

    fix: 監視機能を追加

---
+++ setup.ansible/roles/common/templates/wifi/watch_wifi.zsh
@@ -0,0 +1,29 @@
+#!/usr/bin/env zsh
+
+COUNT=5
+
+
+function get_gateway_ip {
+    for ((i=0; i < 5; i++)); do
+	ip=$(/sbin/ip route | awk '/default/ { print $3 }');
+	if [ "$ip" != "" ]; then
+	    echo $ip
+	    return
+	fi
+    done
+}
+
+gateway_ip=$(get_gateway_ip)
+
+if [ "$gateway_ip" = "" ]; then
+    echo "unable to get gateway IP."
+    /sbin/wpa_cli -i wlan0 reconfigure
+fi
+
+count=$(ping -c $COUNT -q ${gateway_ip} | grep 'received' | awk -F',' '{ print $2 }' | awk '{ print $1 }')
+if [ "$count" = "0" ]; then
+    echo "Connectivity test failed. Let's try to reconnect to WiFi AP."
+    /sbin/wpa_cli -i wlan0 reconfigure
+else
+    echo "Connectivity test passed. Packet(s) received: ${count}."
+fi
